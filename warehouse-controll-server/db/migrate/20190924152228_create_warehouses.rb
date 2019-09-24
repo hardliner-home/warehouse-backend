318facfdf7926b3e142eeeaf16392c4581d272cd
vladimir <hardliner.home@mail.com>
2019-09-24 19:11:41 +0300

    Added Bootstrap by CDN( temporarily)

---
+++ warehouse-controll-server/db/migrate/20190924152228_create_warehouses.rb
@@ -0,0 +1,10 @@
+class CreateWarehouses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :warehouses do |t|
+      t.string :title, :address
+      t.integer :number, :owner_id
+
+      t.timestamps
+    end
+  end
+end
--- warehouse-controll-server/db/schema.rb
+++ warehouse-controll-server/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_130623) do
+ActiveRecord::Schema.define(version: 2019_09_24_145054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +29,8 @@ ActiveRecord::Schema.define(version: 2019_09_24_130623) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
