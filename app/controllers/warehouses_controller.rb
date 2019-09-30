703737f9f7a3220c256b872517a346f670523a67
Vladimir <hardliner.home@mail.ru>
2019-09-30 07:37:52 +0300

    Intermedeate commit

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,31 +1,32 @@
 class WarehousesController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
-  def index
-  end
-
-  def show
-    @title='Warehouses'
-    @warehouses= Warehouse.all
-  end
-
-  def new
-
-  end
-
-  def create
-    @warehouse = Warehouse.new(warehouse_params)
-
-    @warehouse.save
-    redirect_to @warehouse
-  end
-
-  def delete
-
-  end
-
-  private
-    def warehouse_params
-      params.require(:warehouse).permit(:title, :address, :number)
-    end
+  # def index
+  #   @title='Warehouses'
+  #   @warehouses = Warehouse.find(params[:id])
+  # end
+  #
+  # def show
+  #   @title='Warehouses'
+  #   @warehouses = Warehouse.all
+  # end
+  #
+  # def new
+  #
+  # end
+  #
+  # def create
+  #   @warehouse = Warehouse.new(warehouse_params)
+  #   @warehouse.save
+  #   redirect_to @warehouse
+  # end
+  #
+  # def destroy
+  #
+  # end
+  #
+  # private
+  #   def warehouse_params
+  #     params.require(:warehouse).permit(:title, :address, :number)
+  #   end
 end
